63b5d2db86eaebd9a8ac5eb3a65ffd5df4678e5f
Octrian Tito <octriantito@gmail.com>
2025-03-08 15:14:26 +0700

    praktikum 2.5

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -8,12 +8,22 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $user]);
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ]);
+        
+        $user->username = 'manager12';
 
-        // $user = UserModel::findOrFail{1};
-        // return view('user', ['data' => $user]);
+        $user->save();
 
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged('username', 'level_id'); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); // true
         // $user = UserModel::firstOrNew(
         //     [
         //         'username' => 'manager33',

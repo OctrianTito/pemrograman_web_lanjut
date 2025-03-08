5f47a0a16c3c90e69a8c3b5ce8cc0a8e857e820f
Octrian Tito <octriantito@gmail.com>
2025-03-08 07:32:25 +0700

    praktikum 2.3

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -9,10 +8,12 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+
+        $user = UserModel::count('user_id');
         return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();  
+        // return view('user', ['data' => $user]);
 
         // $user = UserModel::find(1);
         // return view('user', ['data' => $user]);
--- PWL_POS/app/Models/UserModel.php
+++ PWL_POS/app/Models/UserModel.php
@@ -12,5 +12,6 @@ class UserModel extends Model
     protected $table = 'm_user';  // Mendefinisikan nama tabel yang digunakan di model ini
     protected $primaryKey = 'user_id';  // Mendefinisikan PK dari tabel yang digunakan
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    // protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['user_id'];
 }

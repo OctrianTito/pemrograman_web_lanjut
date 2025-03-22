e0a201c39a994d0f461028dedb3e3aa27fc83640
Octrian Tito <octriantito@gmail.com>
2025-03-23 00:04:21 +0700

    Penerapan JS 6

--- Minggu6/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu6/PWL_POS/app/Http/Controllers/UserController.php
@@ -210,6 +210,13 @@ class UserController extends Controller
         return redirect('/');
     }
 
+    public function show_ajax(string $id)
+    {
+        $user = UserModel::with('level')->find($id);
+
+        return view('user.show_ajax', ['user' => $user]);
+    }
+
     // Menampilkan edit user ajax
     public function edit_ajax(string $id){
         $user = UserModel::find($id);

16f6ef7a29192d298b45d1f13c0eaa16fc6d868b
Octrian Tito <octriantito@gmail.com>
2025-04-26 21:21:45 +0700

    Tugas Done

--- Minggu11/PWL_POS/app/Http/Controllers/Api/BarangController.php
+++ Minggu11/PWL_POS/app/Http/Controllers/Api/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\BarangModel;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -15,8 +16,44 @@ class BarangController extends Controller
 
     public function store(Request $request)
     {
-        $barang = BarangModel::create($request->all());
-        return response()->json($barang, 201);
+         //set Validation
+         $validator = Validator::make($request->all(), [
+            'kategori_id' => 'required',
+            'barang_kode' => 'required',
+            'barang_nama' => 'required',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        
+        //if validations fails
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // create barang
+        $barang = BarangModel::create([
+            'kategori_id' => $request->kategori_id,
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'image' => $request->image->hashName(),
+        ]);
+
+        // return response JSON user is created
+        if ($barang) {
+            return response()->json([
+                'success' => true,
+                'barang' => $barang,
+            ], 201);
+        }
+
+        // return JSON process insert failed
+        return response()->json([
+            'success' => false,
+        ], 409);
+
     }
 
     public function show(BarangModel $barang)
